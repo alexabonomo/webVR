a3467b699ff8c765ea1c10c560d5d9017f4e45f0
Alexa Ann Bonomo <alexabonomo.art@gmail.com>
2017-06-01 20:24:22 -0700

    Francis changed it a little bit

--- js/main.js
+++ js/main.js
@@ -1,15 +1,22 @@
 AFRAME.registerComponent('sin-y', {
   schema: {
-    offsetY: {type: 'float', default: 0}
+    offsetA: {type: 'float', default: 0},
+    speedX: {type: 'float', default: .1},
+    totalSizeX: {type: 'float'},
+    height: {type: 'float', default: 0}
   },
   tick: function(time, timeDelta) {
     let position = this.el.object3D.position;
-    this.el.object3D.position.setY(Math.sin( ( time * .001 ) + this.data.offsetY));
+    this.el.object3D.position.setX(this.data.speedX + this.el.object3D.position.x);
+    this.el.object3D.position.setY(this.data.height * Math.sin((time * 0.001) + this.data.offsetA));
+
+    if (this.el.object3D.position.x >= this.data.totalSizeX) {
+      this.el.object3D.position.setX(0);
+    }
   }
 });
 
 let scene = document.querySelector('a-scene');
-
 
 let objectCount = 10;
 let elementRadius = 1.5;
@@ -19,38 +26,39 @@ let size = (elementRadius * 2) + elementSpacing;
 let totalSize = objectCount * size;
 
 let containerElement = document.createElement('a-entity');
-containerElement.setAttribute('position', `${- (.5 * totalSize )} 0 ${- (.5 * totalSize )}` );
-scene.appendChild( containerElement);
-
+containerElement.setAttribute('position', `${-totalSize / 2} 0 ${-totalSize / 2}`);
+scene.appendChild(containerElement);
 
 function create3DObject(x, y, z) {
   let element = document.createElement('a-entity');
-  element.setAttribute("geometry", "primitive: sphere; radius: 1.5");
-  element.setAttribute("material", `color:${ getRandomColor() }; metalness: 0; roughness: 0;`);
-  element.setAttribute("position", `${x}, ${y}, ${z}`);
-  element.setAttribute('sin-y', `offsetY: ${y}` );
-
+  element.setAttribute('position', `${x} ${y} ${z}`);
+  element.setAttribute('geometry', 'primitive: sphere; radius: 1.5;');
+  element.setAttribute('material', `color:#${getRandomColor()}; metalness: 0; roughness: 0`);
+  element.setAttribute('sin-y', `height: 5; offsetA: ${y}; totalSizeX: ${totalSize}`);
   containerElement.appendChild(element);
 }
 
 function addObjects() {
-
-
-
-  for( let i=0; i<objectCount; i++) {
-
-    for( let j=0; j<objectCount; j++){
-
-        let y = Math.sin( i + j ) * 5;
-        create3DObject( ( i * size), y, j * size);
+  for (let i = 0; i < objectCount; i++) {
+    for (let j = 0; j < 10; j++) {
+      create3DObject(i * size, 2*Math.PI*i/objectCount + 2*Math.PI*j/objectCount, j * size);
     }
-
   }
 }
 
 addObjects();
 
+// get random hex color
 function getRandomColor() {
-  // There are 16^6 possible hex colors (16777216)
-  return "#" + Math.floor(Math.random() * 16777216).toString(16);
+
+  let letters = '0123456789abcdef';
+
+  let randomColor = '';
+
+  for (let i = 0; i < 6; i++) {
+    randomColor += letters[Math.floor(Math.random() * 16)];
+  }
+
+  return randomColor;
+
 }
