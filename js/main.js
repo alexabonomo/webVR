93124b512a5c49d76e0eae6e61c01fd18cc9d0e4
Alexa Ann Bonomo <alexabonomo.art@gmail.com>
2017-06-01 20:08:23 -0700

    animation

--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-let scene = document.querySelector("a-scene");
+AFRAME.registerComponent('sin-y', {
+  schema: {
+    offsetY: {type: 'float', default: 0}
+  },
+  tick: function(time, timeDelta) {
+    let position = this.el.object3D.position;
+    this.el.object3D.position.setY(Math.sin( ( time * .001 ) + this.data.offsetY));
+  }
+});
+
+let scene = document.querySelector('a-scene');
 
 
 let objectCount = 10;
@@ -13,16 +23,12 @@ containerElement.setAttribute('position', `${- (.5 * totalSize )} 0 ${- (.5 * to
 scene.appendChild( containerElement);
 
 
-function getRandomColor() {
-  // There are 16^6 possible hex colors (16777216)
-  return "#" + Math.floor(Math.random() * 16777216).toString(16);
-}
-
 function create3DObject(x, y, z) {
-  let element = document.createElement("a-entity");
+  let element = document.createElement('a-entity');
   element.setAttribute("geometry", "primitive: sphere; radius: 1.5");
   element.setAttribute("material", `color:${ getRandomColor() }; metalness: 0; roughness: 0;`);
   element.setAttribute("position", `${x}, ${y}, ${z}`);
+  element.setAttribute('sin-y', `offsetY: ${y}` );
 
   containerElement.appendChild(element);
 }
@@ -35,12 +41,16 @@ function addObjects() {
 
     for( let j=0; j<objectCount; j++){
 
-        create3DObject( ( i * size), 0, j * size);
+        let y = Math.sin( i + j ) * 5;
+        create3DObject( ( i * size), y, j * size);
     }
-
-
 
   }
 }
 
 addObjects();
+
+function getRandomColor() {
+  // There are 16^6 possible hex colors (16777216)
+  return "#" + Math.floor(Math.random() * 16777216).toString(16);
+}
